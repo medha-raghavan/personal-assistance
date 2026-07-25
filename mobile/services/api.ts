14fafa2a0f97f466aeed2dd35d91184d788fea0d
Medha <medha.raghavan13@gmail.com>
2026-07-25 14:32:59 +0000

    changes

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import { useAuthStore } from '../store/authStore';
 
-const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:3001/api';
+const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://raspberrypi.tail38a9a8.ts.net:3001/api';
 
 const api = axios.create({
   baseURL: API_URL,
